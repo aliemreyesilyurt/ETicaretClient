0631bcc4cf7cb5a167a22a3ad0aa4536b0b51f8e
Ali Emre Yeşilyurt <99074640+aliemreyesilyurt@users.noreply.github.com>
2024-11-14 17:26:40 +0300

    Alertify was integrated into the project

--- src/app/admin/components/dashboards/dashboards.component.ts
+++ src/app/admin/components/dashboards/dashboards.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from '../../../services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboards',
   templateUrl: './dashboards.component.html',
-  styleUrl: './dashboards.component.scss'
+  styleUrl: './dashboards.component.scss',
 })
-export class DashboardsComponent {
+export class DashboardsComponent implements OnInit {
+  constructor(private alertify: AlertifyService) {}
 
+  ngOnInit(): void {}
+
+  m() {
+    this.alertify.message('Merhaba', {
+      messageType: MessageType.Success,
+      delay: 5,
+      position: Position.TopRight,
+    });
+  }
+
+  d() {
+    this.alertify.dismiss();
+  }
 }
